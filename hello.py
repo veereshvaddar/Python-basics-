01e79baa4e64b9d43ba0e081b39b2d4a2f75f2df
veereshvaddar <veereshvaddar767@gmail.com>
2026-07-14 23:51:56 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,12 +1,2 @@
 num = int(input("Enter your number: "))
 print(num)
-def check_marks():
-    marks = [20, 30, 50, 60, 45, 80]
-
-    for i in marks:
-        if i >= 50:
-            print("Pass")
-        else:
-            print("Fail")
-
-check_marks()
