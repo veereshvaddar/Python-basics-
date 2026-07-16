5796d02251c085ad072a9698e0c8895e4f409df1
veereshvaddar <veereshvaddar767@gmail.com>
2026-07-17 00:03:37 +0530

    Implement check_marks function to evaluate scores
    Fix the logic to check if marks are passing or failing.

---
+++ Check_marks.py
@@ -0,0 +1,9 @@
+def check_marks():
+    marks = [25,35,45,55,65,75]
+    for i in marks:
+        if marks >= 50:
+            print("pass")
+        else:
+            print("fail")
+check_marks()
+           
