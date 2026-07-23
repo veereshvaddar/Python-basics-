6501dbd10d00591e815c37a3cd31aa8967d8fd68
veereshvaddar <veereshvaddar767@gmail.com>
2026-07-23 23:08:32 +0530

    Create Largest_number.py

---
+++ Largest_number.py
@@ -0,0 +1,9 @@
+def largest_number():
+    num = [0,10,20,45,60,25,60]
+    
+    largest number = number(0)
+    for i in numbers:
+        if num > largest 
+           largest = i
+    print("largest number:",largest)
+largest_number()
