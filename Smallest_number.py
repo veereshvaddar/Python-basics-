3d79ba1b2f555c545bcc685807690a0049818d0c
veereshvaddar <veereshvaddar767@gmail.com>
2026-07-23 23:16:11 +0530

    Implement function to find the smallest number
    Fix syntax errors and implement logic to find the smallest number in a list.

---
+++ Smallest_number.py
@@ -0,0 +1,9 @@
+def smallest_number():
+    num = [25,40,55,0,60,]
+
+    smallest number = num(0)
+    for i in num:
+       if < smallest:
+          smallest= i 
+    print("smallest number:", smallest)
+smallest_number()
