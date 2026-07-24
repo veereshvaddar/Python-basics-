bd63b3d1d6e37f7f450f8ed7b1dc2f8e5b5e06ee
veereshvaddar <veereshvaddar767@gmail.com>
2026-07-24 17:26:12 +0530

    Create login.py

---
+++ login.py
@@ -0,0 +1,8 @@
+username = "errorythm"
+password = "1234"
+u = input("enter you username:")
+p = input("enter your password:")
+if u == username and p == password:
+  print("login successfull")
+else:
+  print("login failed")
